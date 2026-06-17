579efa96b1c0d28afdb4173d77f9563e441e453e
ambestenrelocationspro-source <ambestenrelocationspro@gmail.com>
2026-06-17 18:45:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -81,7 +81,8 @@ module.exports = async (req, res) => {
       // Inject Google Search Console verification
       body = body.replace(
         "<head>",
-        `<head>\n<meta name="google-site-verification" content="BZ0xSFTM-yYVgYRfCvQF25PjQp5JWdIK61TO-9ex1Sg" />`
+        `<head>\n<meta name="google-site-verification" content="fSOcnpZoH55xSrGRO4mExxPmSNpZ0rI_hbKrQ5BGUUo" />
+<meta name="google-site-verification" content="8hx683BzFr-XSfv5Cwz3bzwrdhZadCuvYpvD4yiCyug" />`
       );
 
       // Update JobPosting schema dates
